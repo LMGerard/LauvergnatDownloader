0396af6df97e433922631176d4e6ef99db293148
LoupioFR <lmgd@outlook.com>
2021-12-12 19:25:32 +0100

    New update

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ target_dir = join(join(environ['USERPROFILE']), 'Desktop', 'math')
 if not exists(target_dir):
     makedirs(target_dir)
 
-url = '???'
-documents = findall("<a href=\"([^\"]*)\".*class=\"doc\".*>", get(url).text)
+main_url, sub_url = open('url.txt', 'r').read().split('\n')
+
+documents = findall("<a href=\"([^\"]*)\".*class=\"doc\".*>", get(main_url).text)
 for href in documents:
     print('Downloading {}'.format(href))
     dir_name = href.split('/')
     sub_target_dir = join(target_dir, *dir_name[:-1])
 
     makedirs(sub_target_dir, exist_ok=True)
-    r = get('???' + href, allow_redirects=True)
+    r = get(sub_url + href, allow_redirects=True)
     open(join(sub_target_dir, dir_name[-1]), 'wb').write(r.content)
 startfile(target_dir)
